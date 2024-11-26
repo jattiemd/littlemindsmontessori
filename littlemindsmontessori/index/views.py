a91fbef8c1529371a8c984c0feeb136c6f80c666
Abdu-Daiyaan Jattiem <AbduJ@biovac.co.za>
2024-11-26 10:15:37 +0200

    updated files

--- littlemindsmontessori/index/views.py
+++ littlemindsmontessori/index/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Content from the Index view.")
+    return render(request, "index/index.html",)
