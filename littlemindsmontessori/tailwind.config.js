73f618ae90e6391e99f503243df484f911144f02
jattiemd <jattiemd@gmail.com>
2025-01-27 09:28:48 +0200

    extra activities and daily program added

---
+++ littlemindsmontessori/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'index/templates/index/**/*.html',
+  ],
+  theme: {
+    fontFamily: {},
+    extend: {},
+  },
+  plugins: [],
+}
+
